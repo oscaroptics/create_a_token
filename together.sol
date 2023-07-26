ac24a9700c9cea962f8d551627c53b8bdb6e4c56
A R Ashutosh <93474166+oscaroptics@users.noreply.github.com>
2023-07-26 22:56:49 +0530

    Create together.sol

---
+++ together.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Together{
+    string public TokenName;
+    string public TokenAbbr;
+    uint public TotalSuppply;
+
+    mapping (address => uint) public balances;
+
+    function mint(address userAdress, uint value)  public {
+        TotalSuppply = TotalSuppply + value;
+        balances[userAdress] = balances[userAdress] + value;
+    }
+
+    function burn(address userAddress, uint value) public {
+        require(balances[userAddress]>=value);
+        TotalSuppply = TotalSuppply-value;
+        balances[userAddress] = balances[userAddress]-value;
+    }
+}
